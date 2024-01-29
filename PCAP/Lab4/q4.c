355a80fe675bf24e29af010ec1d919234441693e
Satwik <113183355+satwikambashta@users.noreply.github.com>
2024-01-29 09:12:28 +0530

    biblically accurate solution :)

--- PCAP/Lab4/q4.c
+++ PCAP/Lab4/q4.c
@@ -1,33 +1,16 @@
 //4x4 matrix and display output using 4 processes
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpi.h"
 
-void ErrorHadler(int error_code) {
-    if (error_code != MPI_SUCCESS)
-    {
-        char error_string[BUFSIZ];
-        int length_of_error_string, error_class;
-
-        MPI_Error_class(error_code, &error_class);
-        MPI_Error_string(error_code, error_string, &length_of_error_string);
-        printf("%d %s\n", error_code, error_string);
-    }
-}
-
 int main(int argc, char *argv[]) {
-    int rank, size,error_code, mat[3][3], *array, ele, count, comcount;
+    int rank, size,error_code, mat[3][3], *array;
     float answer,sum;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPI_Errhandler_set(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
-    error_code = MPI_Comm_size(3, &size);
-    ErrorHadler(error_code);
-    count=0;
     if(rank==0)
     {
         printf("Enter matrix: \n");
