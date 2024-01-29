c2c13cda62996753c1c9476455eb162d69e460ab
Satwik <113183355+satwikambashta@users.noreply.github.com>
2024-01-29 09:14:37 +0530

    correct probably

--- PCAP/Lab4/q4.c
+++ PCAP/Lab4/q4.c
@@ -5,7 +5,7 @@
 #include "mpi.h"
 
 int main(int argc, char *argv[]) {
-    int rank, size,error_code, mat[3][3], *array;
+    int rank, size, mat[4][4], *array;
     float answer,sum;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
