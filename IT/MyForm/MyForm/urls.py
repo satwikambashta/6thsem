793fea19c0b4480b58c2950f2be3fb633557300d
Satwik <113183355+satwikambashta@users.noreply.github.com>
2024-02-06 11:05:18 +0530

    Delete IT/MyForm directory

--- IT/MyForm/MyForm/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, re_path
-#from django.conf.urls import include, url
-from django.conf.urls import include
-urlpatterns = [
- path(r'^admin/', admin.site.urls),
- re_path('', include('formapp.urls')),
- #url('', include('formapp.urls')),
-]
